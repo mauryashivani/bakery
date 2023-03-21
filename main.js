adb95243f2dbeecebed4fa7169bc466d94f20ea8
mauryashivani <126226302+mauryashivani@users.noreply.github.com>
2023-03-21 13:25:57 +0530

    file upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// cart
+// let cartIcon = document.querySelector('#cart-icon');
+// let cart = document.querySelector('.cart');
+// let closeCart = document.querySelector('#close-cart');
+
+// // open
+// cartIcon.onclick = () =>{
+// 	cart.classList.add('active');
+// };
+
+// // close
+// closCart.onclick = () =>{
+// 	cart.classList.remove('active');
+// };
