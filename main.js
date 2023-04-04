e1d4eae0f4da5cac2a6ac2ce3abb440335d10f53
mauryashivani <126226302+mauryashivani@users.noreply.github.com>
2023-04-04 12:11:58 +0530

    file upload

--- main.js
+++ main.js
@@ -1,14 +1,48 @@
 // cart
-// let cartIcon = document.querySelector('#cart-icon');
-// let cart = document.querySelector('.cart');
-// let closeCart = document.querySelector('#close-cart');
 
-// // open
-// cartIcon.onclick = () =>{
-// 	cart.classList.add('active');
-// };
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('.close-cart');
 
-// // close
-// closCart.onclick = () =>{
-// 	cart.classList.remove('active');
-// };
+cartIcon.onclick = () => {
+	cart.classList.add("active");
+};
+
+
+closeCart.onclick = () => {
+	cart.classList.remove("active");
+};
+
+
+// cart working
+
+if(document.readyState == "loading") {
+	document.addEventListener("DOMContentLoader",ready);
+
+} else{
+	ready();
+}
+
+// making function
+function ready(){
+	// remove items from cart
+
+	var removeCartButtons = document.getElementsByClassName('cart-remove')
+	console.log(removeCartButtons)
+	for(var i = 0; i < removeCartButtons.length; i++){
+		var button = removeCartButtons[i];
+		button.addEventListener("click",removeCartItem);
+	}
+}
+ 
+// remove item from cart
+function removeCartItem(event){
+	var buttonClicked = event.target;
+	buttonClicked.parrentElement.remove();
+}
+
+// update total
+function updatetotal(){
+	var cartContent = document.getElementsByClassName('cart-content')[0]
+	var cartBox = cartContent.getElementsByClassName
+} 
