6207ef6ae15b7ee80f11c81bad2ee175a4af5afe
mauryashivani <126226302+mauryashivani@users.noreply.github.com>
2023-04-06 13:10:29 +0530

    upload file

--- main.js
+++ main.js
@@ -2,15 +2,15 @@
 
 let cartIcon = document.querySelector('#cart-icon');
 let cart = document.querySelector('.cart');
-let closeCart = document.querySelector('.close-cart');
+let closeCart = document.querySelector("#close-cart");
 
 cartIcon.onclick = () => {
-	cart.classList.add("active");
+  cart.classList.add("active");
 };
 
 
 closeCart.onclick = () => {
-	cart.classList.remove("active");
+  cart.classList.remove("active");
 };
 
 
@@ -34,15 +34,53 @@ function ready(){
 		button.addEventListener("click",removeCartItem);
 	}
 }
+
+// quantity changes
+
+// var quantityInputs = document.getElementsByClassName('cart-quantity')
+// for (var i = 0; i < quantityInputs.length; i++){
+// 	var input = quantityInputs[i]
+// 	input.addEventListener("changed",quantityChanged);
+
+// }
  
 // remove item from cart
-function removeCartItem(event){
-	var buttonClicked = event.target;
-	buttonClicked.parrentElement.remove();
-}
+// function removeCartItem(event){
+// 	var buttonClicked = event.target;
+// 	buttonClicked.parentElement.remove();
+// 	updatetotal();
+//  }
+
+// quantity change
+// function quantityChanged(event){
+// 	var input = event.target
+// 	if (isNaN(input.value)) || input.value <= 0{
+// 		input.value = 1
+// 	}
+// 	updatetotal();
+// }
+
+
 
 // update total
-function updatetotal(){
-	var cartContent = document.getElementsByClassName('cart-content')[0]
-	var cartBox = cartContent.getElementsByClassName
-} 
+// function updatetotal(){
+// 	var cartContent = document.getElementsByClassName('cart-content')[0];
+// 	var cartBox = cartContent.getElementsByClassName('cart-box');
+// 	var total = 0;
+// 	for (var i=0; i < cartBoxes.length; i++){
+// 		var cartBox = cartBoxes[i];
+// 		var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+// 		var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+// 		var price = parseFloat(priceElement.innerText.replace("$",""));
+// 		var quantity = quantityElement.value;
+// 		total = total + (price * quantity);
+
+		
+
+// 		total = Math.round(total*100)/100;
+// 		document.getElementsByClassName('total-price')[0].innerText = '$' + total;
+
+
+// 	}
+
+// } 
