61a19790c1e1871d9823b5b463ce6fdfda15201d
mauryashivani <126226302+mauryashivani@users.noreply.github.com>
2023-04-21 13:15:41 +0530

    file upload

--- main.js
+++ main.js
@@ -1,48 +1,42 @@
 // cart
 
-let cartIcon = document.querySelector('#cart-icon');
-let cart = document.querySelector('.cart');
-let closeCart = document.querySelector('.close-cart');
-
-cartIcon.onclick = () => {
-	cart.classList.add("active");
-};
-
-
-closeCart.onclick = () => {
-	cart.classList.remove("active");
-};
-
-
-// cart working
-
-if(document.readyState == "loading") {
-	document.addEventListener("DOMContentLoader",ready);
-
-} else{
-	ready();
+function displayCart(){
+	document.getElementById('cart').style.display ='block'
 }
 
-// making function
-function ready(){
-	// remove items from cart
 
-	var removeCartButtons = document.getElementsByClassName('cart-remove')
-	console.log(removeCartButtons)
-	for(var i = 0; i < removeCartButtons.length; i++){
-		var button = removeCartButtons[i];
-		button.addEventListener("click",removeCartItem);
-	}
-}
- 
-// remove item from cart
-function removeCartItem(event){
-	var buttonClicked = event.target;
-	buttonClicked.parrentElement.remove();
+
+
+
+
+
+
+function addtoCart(id)
+{
+	displayCart();
 }
 
-// update total
-function updatetotal(){
-	var cartContent = document.getElementsByClassName('cart-content')[0]
-	var cartBox = cartContent.getElementsByClassName
-} 
+
+// let cartIcon = document.querySelector('#cart-icon');
+// let cart = document.querySelector('.cart');
+// let closeCart = document.querySelector("#close-cart");
+
+// cartIcon.onclick = () => {
+//   cart.classList.add("active");
+// };
+
+
+// closeCart.onclick = () => {
+//   cart.classList.remove("active");
+// };
+
+
+// if (document.readyState == 'loading'){
+// 	document.addEventListner("DOMContentLoaded",ready);
+// } else{
+// 	ready();
+// }
+// // making function
+// function ready(){
+// 	// remove item from cart
+	
