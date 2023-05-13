64576f68e394de0d9d0a9f08edd38792070071e4
mauryashivani <126226302+mauryashivani@users.noreply.github.com>
2023-05-13 13:16:21 +0530

    file upload

--- main.js
+++ main.js
@@ -1,42 +1,11 @@
-// cart
 
-function displayCart(){
-	document.getElementById('cart').style.display ='block'
+
+function displayproduct(){
+	document.getElementById()
 }
 
 
-
-
-
-
-
-
-function addtoCart(id)
+function addtocart(id)
 {
-	displayCart();
+	displayProduct('cart').style.display = 'flex'
 }
-
-
-// let cartIcon = document.querySelector('#cart-icon');
-// let cart = document.querySelector('.cart');
-// let closeCart = document.querySelector("#close-cart");
-
-// cartIcon.onclick = () => {
-//   cart.classList.add("active");
-// };
-
-
-// closeCart.onclick = () => {
-//   cart.classList.remove("active");
-// };
-
-
-// if (document.readyState == 'loading'){
-// 	document.addEventListner("DOMContentLoaded",ready);
-// } else{
-// 	ready();
-// }
-// // making function
-// function ready(){
-// 	// remove item from cart
-	
